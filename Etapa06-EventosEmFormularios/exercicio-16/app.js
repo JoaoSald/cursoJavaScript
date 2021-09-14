e3d52b03fe5871f3080c9086ec0fc3a08200888e
Joao Victor Correa Saldanha <joaosaldanha388@gmail.com>
2021-09-13 22:51:22 -0300

    exercicioFeito

--- Etapa06-EventosEmFormularios/exercicio-16/app.js
+++ Etapa06-EventosEmFormularios/exercicio-16/app.js
@@ -5,18 +5,18 @@
     'Clicou na div.' não seja exibida no console.
 */
 
-const div = document.querySelector('div')
-const elementsInsideDiv = Array.from(div.children)
+// const div = document.querySelector('div')
+// const elementsInsideDiv = Array.from(div.children)
 
-elementsInsideDiv.forEach(element => {
-  element.addEventListener('click', () => {
-    console.log('Clicou no filho da div.')
-  })
-})
+// elementsInsideDiv.forEach(element => {
+//   element.addEventListener('click', (event) => {
+//     console.log('Clicou no filho da div.')
+//   })
+// })
 
-div.addEventListener('click', () => {
-  console.log('Clicou na div.')
-})
+// div.addEventListener('click', () => {
+//   console.log('Clicou na div.')
+// })
 
 /*
   02
@@ -26,13 +26,52 @@ div.addEventListener('click', () => {
     da div.".
 */
 
+// const div = document.querySelector('div')
+// div.addEventListener('click', ()=>{
+//   console.log("clicou!!!")
+// })
+
+// const divFilhos = Array.from(div.children)
+
+// divFilhos.forEach((item)=>{
+
+//   item.addEventListener('click', (element)=>{
+//     console.log(`Clicou no ${element.target}, filho
+//     da div.`)
+//   })
+
+// })
+
 /*
   03
 
   - No index.html, abaixo da div sem classe, insira um h2;
   - Faça com que a mensagem de clique na div e a mensagem de clique em algum
     filho da div, ao invés de ser exibida no console, seja inserida neste h2.
-*/
+// */
+// const div = document.querySelector('div')
+// const divFilhos = Array.from(div.children)
+
+// div.addEventListener('click', ()=>{
+
+//   div.innerHTML += '<h2 class="teste-h2"> H2 inserido </h2>'
+ 
+// })
+
+// const h2 = document.querySelector('.teste-h2')
+
+// h2.addEventListener('click', ()=>{
+
+//   console.log('ola')
+ 
+// })
+
+//  divFilhos.forEach((item)=>{
+//    item.addEventListener('click', ()=>{
+    
+//    })
+//  })
+
 
 /*
   04
@@ -48,6 +87,10 @@ div.addEventListener('click', () => {
     o texto que ela tem por 
     "Eixo X: COORDENADA_EIXO_X | Eixo Y: COORDENADA_EIXO_Y".
 */
+const moveEgg = document.querySelector('.egg')
+moveEgg.addEventListener('mousemove', (event)=>{
+  console.log(event.offsetX, event.offsetY)
+})
 
 /*
   06
@@ -55,6 +98,12 @@ div.addEventListener('click', () => {
   - Modifique a cor do ovo para "lightgoldenrodyellow" quando o botão for 
     clicado.
 */
+const button = document.querySelector('button')
+const divEgg = document.querySelector('.egg')
+
+button.addEventListener('click', ()=>{
+divEgg.style.background = 'lightgoldenrodyellow'
+})
 
 /*
   07
@@ -76,3 +125,9 @@ const people = [
   { id: 8, name: 'Matheus Manucci', profession: 'Piloto' },
   { id: 9, name: 'Hamilton Silva', profession: 'Advogado' }
 ]
+
+people.forEach(item=>{
+  if(item.profession == 'Front-end developer'){
+    console.log( "O array people contém, no mínimo, um(a) Front-end developer.")
+  }
+})
