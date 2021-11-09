3127d90141e21ce0fcd199d2fd18edaaba1d34db
Joao Victor Correa Saldanha <joaosaldanha388@gmail.com>
2021-11-09 00:49:55 -0300

    metodo reduce

---
+++ Etapa08-MetodosDeArray/aula 01-04_metodoReduce/app.js
@@ -0,0 +1,29 @@
+//metodo reduce usamos para reduzir algum dado
+const numbers = [1, 2, 3]
+const someReduce = numbers.reduce( (accumulator, item )=> accumulator + item, 0) // zero é o valor inical do accumulator 
+// accumulator armazena o valor do array item por item 
+console.log(someReduce)
+
+const phaseScores = [
+    { name: 'Vinicius Costa', score: 337 },
+    { name: 'Roger Melo', score: 43 },
+    { name: 'Alfredo Braga', score: 234 },
+    { name: 'Pedro H. Silva', score: 261 },
+    { name: 'Ana Paula Rocha', score: 491 },
+    { name: 'Vinicius Costa', score: 167 },
+    { name: 'Roger Melo', score: 137 },
+    { name: 'Alfredo Braga', score: 135 },
+    { name: 'Ana Paula Rocha', score: 359 },
+    { name: 'Pedro H. Silva', score: 133 }
+  ]
+
+  const rogerScore = phaseScores.reduce( (accumulator, phaseScore)=>{
+            if (phaseScore.name === 'Roger Melo')
+            {
+                accumulator += phaseScore.score
+            }
+
+        return accumulator
+  }, 0)
+
+  console.log(rogerScore)
