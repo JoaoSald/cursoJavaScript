0ea1df50278227514e129cb3274ebf9495c601cc
Joao Victor Correa Saldanha <joaosaldanha388@gmail.com>
2022-01-13 11:32:48 -0300

    add todo

--- Etapa09-aplicaçãoToDoList/aula01-03_adicionandoToDos/app.js
+++ Etapa09-aplicaçãoToDoList/aula01-03_adicionandoToDos/app.js
@@ -1,1 +1,18 @@
+const formAddTodo = document.querySelector('.form-add-todo')
+const todosContainer = document.querySelector('.todos-container')
 
+formAddTodo.addEventListener('submit', evento => {
+    evento.preventDefault()
+
+    const inputValue = evento.target.add.value.trim() // metodo trim remove espaços em branco no inicio/fim
+    if(inputValue.length){
+          todosContainer.innerHTML += `  
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${inputValue}</span>
+    <i class="far fa-trash-alt delete"></i>
+  </li>`
+
+  evento.target.reset() //limpa o input
+    }
+  
+})
