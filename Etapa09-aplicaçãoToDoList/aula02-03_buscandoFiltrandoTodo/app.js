96aae5c7d88a843e68c121f6e17dd6288e795c00
Joao Victor Correa Saldanha <joaosaldanha388@gmail.com>
2022-01-15 18:14:08 -0300

    add coments

--- Etapa09-aplicaçãoToDoList/aula02-03_buscandoFiltrandoTodo/app.js
+++ Etapa09-aplicaçãoToDoList/aula02-03_buscandoFiltrandoTodo/app.js
@@ -25,13 +25,16 @@ todosContainer.addEventListener('click', evento =>{
 })
 
 inputSearchTodo.addEventListener('input', evento =>{
-      const inputValue = evento.target.value.trim()
+      const inputValue = evento.target.value.trim() //obtendo a referencia do valor inserido no input(fornecido pelo usuario)
+      // Array.from transforma o htmlCollection em array
+      // temos a ref da ul na const todosContainers e para manipularmos o conteudo dessa ul usamos o .children, que pega todos os filhos da ul
       Array.from(todosContainer.children)
-      .filter( todo =>  !todo.textContent.includes(inputValue))
-      .forEach(todo => {
-        todo.classList.remove('d-flex')
-        todo.classList.add('hidden')
-      })
+      .filter( todo =>  !todo.textContent.includes(inputValue)) //filtrando o conteúdo diferente do inputValue(fornecido pelo usuario) e retornando ele num array 
+      .forEach(todo => { //aqui vamos iterar esses valores que foram filtrados e adicioanar ou remover alguma clase
+        todo.classList.remove('d-flex') //removendo a classe d-flex que sobrescreve a hidden
+        todo.classList.add('hidden') // adicionando a classe hidden nos valores q nao dao match com o inputValue, assim removendo eles da tela   
+      }) 
+      //nesse bloco fazemos o contrario do código a cima 
       Array.from(todosContainer.children)
       .filter( todo =>  todo.textContent.includes(inputValue))
       .forEach(todo => {
