015349f0bf696227e9e1e05eddf56ed562e2b559
JoaoSald <joaosaldanha388@gmail.com>
2023-06-02 18:35:21 -0300

    get info cidade

--- Etapa12-aplicaçãoWeatherApplication/js/weather.js
+++ Etapa12-aplicaçãoWeatherApplication/js/weather.js
@@ -1,1 +1,21 @@
-const APIKey = '4Ggg5RaPMRPGgx70ZiUo5fxOWfqaMlHH'
+const APIKey = 'C9EyqsVd7kd1WW0mWAd8XAqAM83bpOyw'
+const getCityUrl = cityName => 
+`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getCityData = async (cityName) =>{
+    try {
+      const cityUrl = getCityUrl(cityName)
+      const response = await fetch(cityUrl)
+
+      if(!response.ok){
+        throw new Error('Não foi possivel obter dados')
+      }
+
+      const [cityData] = await response.json()
+     return cityData
+    }catch ({name, message}){
+        alert(`${name}: ${message}`)
+    }
+}
+
+getCityData("São Paulo")
